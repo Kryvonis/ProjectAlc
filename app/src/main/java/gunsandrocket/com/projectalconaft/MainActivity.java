a96f9acdece9a493f810154090f15d950143b3ee
ArtGizmo <artkryvonis@gmail.com>
2015-06-23 16:53:06 +0300

    initial comit

--- app/src/main/java/gunsandrocket/com/projectalconaft/MainActivity.java
+++ app/src/main/java/gunsandrocket/com/projectalconaft/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+     int test = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
