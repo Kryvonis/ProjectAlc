d693c7e7803338b132e78854f596afd00958b6ae
ArtGizmo <artkryvonis@gmail.com>
2015-06-23 17:00:08 +0300

    created int test2

--- app/src/main/java/gunsandrocket/com/projectalconaft/MainActivity.java
+++ app/src/main/java/gunsandrocket/com/projectalconaft/MainActivity.java
@@ -8,12 +8,10 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
      int test = 0;
-     int test2 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int testDimon = 0;
     }
 
     @Override
